032c5e20428a083d055e81e791188526bcea912d
Yuri47 <yuri.alexs@gmail.com>
2018-05-30 17:56:01 -0300

    vue

---
+++ Vue/src/components/button.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  methods: {
+    mudarDados() {
+      window.console.log('somebody')
+    }
+  }
+}
+</script>
+
+<template id="">
+  <div class="">
+    <button @click="mudarDados">Mudar Dados</button>
+  </div>
+</template>
---
+++ Vue/src/store/state.js
@@ -0,0 +1,9 @@
+
+export default {
+    user: {
+      name: 'Yuri',
+      email: 'yuri.alexs@gmail.com',
+      level: '47'
+    },
+    token: '1654747474898748454g'
+}
